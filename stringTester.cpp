5a8d8452a7a730bd6dea0e0d19409af3a65bea3c
Nayana Limaye <nalimaye@yahoo.com>
2020-08-15 20:52:23 -0400

    Added functionality to StringTester

--- stringTester.cpp
+++ stringTester.cpp
@@ -11,6 +11,8 @@ int main()
   string farewell("\nThank you for playing !\n\n");
   string phrase, subPhrase = "";
   int position = 0;
+  char charToReplace;
+  int numCharsToErase = 0;
 
   cout << greeting;
   cout << "\nPlease input a phrase to test : ";
@@ -18,6 +20,7 @@ int main()
 
   cout << "Your phrase has " << phrase.size() << " characters in it.\n";
 
+  // Ask player for a sub-phrase to search.
   cout << "\nPlease enter a sub-phrase to search : ";
   getline(cin, subPhrase);
   position = phrase.find(subPhrase);
@@ -29,6 +32,23 @@ int main()
   {
     cout << "Sub-phrase found at position : " << position << "\n";
   }
+
+  // Ask player a position to replace a character at.
+  cout << "\nPosition to replace a character at : ";
+  cin >> position;
+  cout << "Character at position " << position << " is " << phrase[position] << "\n";
+  cout << "Character to replace : ";
+  cin >> charToReplace;
+  phrase[position] = charToReplace;
+  cout << "Now phrase is : " << phrase << "\n";
+
+  // Ask player a position to erase character(s) at and how many character(s).
+  cout << "\nPosition to erase character(s) at : ";
+  cin >> position;
+  cout << "How many character(s) to erase : ";
+  cin >> numCharsToErase;
+  phrase.erase(position, numCharsToErase);
+  cout << "Now phrase is : " << phrase << "\n";
 
   cout << farewell;
   return 0;
